fe4a43cab2695fe9419ac4cf01c2b5596c14f66f
Sourav <souravsunju@gmail.com>
2021-05-16 16:33:19 +0530

    new tab added

---
+++ newspaper-startup/script.js
@@ -0,0 +1,13 @@
+var date = document.getElementById("dateFeild");
+
+
+var currentdate = new Date(); 
+var datetime = currentdate.getDate() + "/"
+                + (currentdate.getMonth()+1)  + "/" 
+                + currentdate.getFullYear() + " @ "  
+                + currentdate.getHours() + ":"  
+                + currentdate.getMinutes() + ":" 
+                + currentdate.getSeconds();
+
+
+date.innerText = datetime;
